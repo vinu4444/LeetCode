29cd54ee6a8983c83a8cb2a31c9d1678d612d03d
G Vinuthna <gopidesivinuthna@gmail.com>
2025-09-21 16:17:44 +0530

    Time: 1163 ms (8.29%) | Memory: 59.5 MB (75.30%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n=nums.length;
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<=i+k && j<n;j++){
+                if (nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

dac29cd047fa3575051be83e6cf6b4e159517f69
G Vinuthna <gopidesivinuthna@gmail.com>
2025-09-27 21:08:35 +0530

    Time: 25 ms (64.48%) | Memory: 40.7 MB (28.04%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,11 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l=0,h=n;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(isBadVersion(mid)) h = mid-1;
+            else l = mid+1;
+        }
+        return l;
+    }
+}
